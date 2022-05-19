e3fbc1872f6c7c3be85aba46e10175e45f4e72f2
Henryk <henipiter@wp.pl>
2022-05-19 19:03:46 +0200

    second solution of show plots

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from matplotlib import pyplot as plt
-
 import model
 
 
@@ -26,29 +25,32 @@ def two_plots_and_write(data1, data2, color1='ro', color2='bo'):
 
 
 def draw_in_compare_mode():
-    col1, col2 = st.columns(2)
+    original_slider_column, both_plots_column, compared_slider_column = st.columns([1, 3, 1])
     freeze_original = st.sidebar.checkbox('Freeze original')
 
-    with col1:
-        st.header("Actual")
+    with original_slider_column:
         freq = st.slider('Frequency [V]', 0.0, 10.0, 5.0, step=0.1, disabled=freeze_original)
-        another = st.slider('Antorhe', 0.0, 10.0, 5.0, step=0.1, disabled=freeze_original)
+        another = st.slider('Another', 0.0, 10.0, 5.0, step=0.1, disabled=freeze_original)
 
-        with st.expander("See original"):
-            plot_and_write(model.get_plot_data(freq), 'ro')
-    with col2:
-        st.header("Freezed")
+        st.error("Original")
 
-        freeze_freq = get_states_variables("freq", "0.5")
-        freeze_another = get_states_variables("another", "0.5")
+    with compared_slider_column:
+        freeze_freq = get_states_variables("Frequency [V]", "0.5")
+        freeze_another = get_states_variables("Another", "0.5")
         freeze_freq = st.slider(label="freeze_freq", value=float(freeze_freq), step=0.1, min_value=0.0, max_value=10.0)
         freeze_another = st.slider(label="freeze_another", value=float(freeze_another), step=0.1, min_value=0.0,
                                    max_value=10.0)
-        with st.expander("See compare"):
-            plot_and_write(model.get_plot_data(float(freeze_freq)), 'bo')
 
-    with st.expander("See combined"):
+        st.info("Compared")
+    with both_plots_column:
         two_plots_and_write(model.get_plot_data(freq), model.get_plot_data(float(freeze_freq)))
+
+    original_plot_column, compared_plot_column = st.columns(2)
+    with original_plot_column:
+        plot_and_write(model.get_plot_data(freq), 'ro')
+    with compared_plot_column:
+        plot_and_write(model.get_plot_data(float(freeze_freq)), 'bo')
+
 
 
 def draw_in_normal_mode():
